691ca587c6288634d8f4cd64da0d134fb6158765
Keydi12 <12keydirodriguez@gmail.com>
2020-06-27 22:21:22 -0600

    CreacionRutas_Keydi

--- bckcuanticos/routes/api/gestion/index.js
+++ bckcuanticos/routes/api/gestion/index.js
@@ -4,11 +4,22 @@ let router = express.Router();
 
 //gestion - Rutas para Usuario Administrador
 
+///	GET DE PAGINA PRINCIPAL DEL ADMINISTRADOR
+router.get('/admin', async (req, res)=>{
+    res.status(403).json({ "msg":"Administrador"});
+}); //get /admin
+
 
 //GET SOLICITUDES DE CANASTAS PREDETERMINADAS
 router.get('/soliPredt', async (req, res)=>{
     res.status(403).json({ "msg":"Solicitudes Canastas Predeterminadas -> NO IMPLEMENTADO"});
 }); //get /soliPredt
+
+
+//PUT DE CAMBIAR ESTADO A FACTURA ESPECIFICA DE OTRAS DONACIONES 
+router.put('/soliOtras/:facCod', async (req, res)=>{
+    res.status(403).json({ "msg":"Cambiar a estado REALIZADA a una Factura específica de Otras Donaciones"});
+}); //put /soliOtras/:facCod
 
 
 module.exports = router;
